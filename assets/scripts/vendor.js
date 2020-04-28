22ed8e794088828a8c028cf8c54c89a65d6eff72
ry12300 <rossyoung990@gmail.com>
2020-04-28 16:08:24 +0100

    add comments to vendor.js || add comments to index.html

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,3 +1,4 @@
+//constants
 const userInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
